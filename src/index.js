f7aff6c6a10ddd3e6a0b97a5e4f9ebc0f6d50fa8
Julia Kaidashova <julia.kaidashova5@gmail.com>
2023-06-01 19:20:00 +0300

    Updated time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Odesa&key=${apiKe
 
 axios.get(apiUrl).then(displayTemperature);
 
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if(minutes<10){
+    minutes = `0${minutes}`;
+  }
+   if (hours < 10) {
+     hours = `0${hours}`;
+   }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response){
  console.log(response.data.temperature.current);
  let temperatureElement = document.querySelector("#temperature");
@@ -10,9 +33,11 @@ function displayTemperature(response){
  let descriptionElement = document.querySelector("#description");
  let humidityElement = document.querySelector("#humidity");
  let windElement = document.querySelector("#wind");
+ let dateElement = document.querySelector("#date");
  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
  cityElement.innerHTML = response.data.city;
  descriptionElement.innerHTML = response.data.condition.description;
  humidityElement.innerHTML = response.data.temperature.humidity;
  windElement.innerHTML = Math.round(response.data.wind.speed);
+ dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
