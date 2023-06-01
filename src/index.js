d5f3387aeabb00b38ab7dc30f95b647d1a5aa48a
Julia Kaidashova <julia.kaidashova5@gmail.com>
2023-06-01 22:42:44 +0300

    adeded multiple forecast

--- src/index.js
+++ src/index.js
@@ -22,6 +22,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+  let forecast = document.querySelector("#forecast");
+  let days = ["Thu", "Fri","Sat"];
+  let forecastHTML= `<div class="row">`
+  days.forEach(function(day){
+    forecastHTML= forecastHTML+
+    `
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png" width="36">
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">18°</span>
+                <span class="weather-forecast-temperature-min">12°</span>
+              </div>
+            </div>
+    `
+  })
+
+  forecastHTML = forecastHTML + `</div>`
+  forecast.innerHTML = forecastHTML;
+}
+
+displayForecast();
 function displayTemperature(response) {
   celsiusTemperature = response.data.temperature.current;
   let temperatureElement = document.querySelector("#temperature");
