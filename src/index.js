65ca8fbbfac3c53dd7b2293cb4b76ee97bd42176
Julia Kaidashova <julia.kaidashova5@gmail.com>
2023-06-01 19:33:12 +0300

    added relevant icon

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 let apiKey = "e981ee31a3f3cof0400b42t3a1bfad08";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Odesa&key=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
-function formatDate(timestamp){
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  if(minutes<10){
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-   if (hours < 10) {
-     hours = `0${hours}`;
-   }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -20,23 +21,26 @@ function formatDate(timestamp){
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
-  ]
+    "Saturday",
+  ];
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`
+  return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response){
- let temperatureElement = document.querySelector("#temperature");
- let cityElement = document.querySelector("#city");
- let descriptionElement = document.querySelector("#description");
- let humidityElement = document.querySelector("#humidity");
- let windElement = document.querySelector("#wind");
- let dateElement = document.querySelector("#date");
- temperatureElement.innerHTML = Math.round(response.data.temperature.current);
- cityElement.innerHTML = response.data.city;
- descriptionElement.innerHTML = response.data.condition.description;
- humidityElement.innerHTML = response.data.temperature.humidity;
- windElement.innerHTML = Math.round(response.data.wind.speed);
- dateElement.innerHTML = formatDate(response.data.time * 1000);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
